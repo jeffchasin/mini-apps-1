1d8f01ec201f98833678bdcb523526bb41795b2d
Jeff Chasin <jeff@jeffchasin.com>
2018-10-12 22:32:20 +0200

    render board to index.html

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,42 @@
+'use strict';
 
+var board;
+
+var createBoardArray = function () {
+  let board = [];
+  for (let i = 0; i < 3; i++) {
+    let row = new Array(3);
+    board.push(row);
+  };
+  return board;
+};
+
+var createRowElements = function () {
+  var row = document.createElement('div');
+  row.className = 'row';
+  for (let i = 0; i < 3; i++) {
+    var btn = document.createElement('button');
+    row.append(btn);
+  }
+  return row;
+};
+
+var renderBoard = function () {
+  let board = document.createElement('div');
+  board.className = 'board';
+  for (let i = 0; i < 3; i++) {
+    let aRow = createRowElements();
+    board.appendChild(aRow);
+  }
+  return board;
+};
+
+var init = function () {
+  let aBoard = renderBoard();
+  document.querySelector('#app').appendChild(aBoard);
+};
+
+window.onload = function () {
+  init();
+};
 
