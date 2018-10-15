faa7781b2872750dfa7fac0b7da094ce15de832d
Jeff Chasin <jeff@jeffchasin.com>
2018-10-14 21:45:16 -0400

    compile app.js with Babel

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class Checkout extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    return (
+      <div>
+        <p>Hello React</p>
+      </div>
+    );
+  }
+
+}
+
+export default Checkout;
+
+ReactDOM.render(
+  <Checkout />,
+  document.getElementById('app')
+);
