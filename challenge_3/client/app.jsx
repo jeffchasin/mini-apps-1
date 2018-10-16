a5612a9efcbaf53a37929ecc37dcb485cfada8aa
Jeff Chasin <jeff@jeffchasin.com>
2018-10-15 20:42:16 -0400

    add babel and react to render client

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
+var Form = function (props) {
+  return (
+    <div>
+      <button onClick={props.handleCheckout}>
+        Checkout
+      </button>
+    </div>
+  );
+};
 
 class Checkout extends React.Component {
   constructor(props) {
     super(props);
+    this.handleCheckout = this.handleCheckout.bind(this);
+    this.state = { form: 'index' };
+  }
+
+  handleCheckout() {
+    this.setState({ form: 'f1' });
+    console.log('this.state.form: ', this.state.form);
   }
 
   render() {
-    return (
-      <div>
-        <p>Hello React</p>
-      </div>
-    );
+    const formPage = this.state.form;
+
+    if (formPage === 'index') {
+      return (
+        <Form onClick={this.handleCheckout} />
+      );
+    }
   }
 
 }
-
-export default Checkout;
 
 ReactDOM.render(
   <Checkout />,
