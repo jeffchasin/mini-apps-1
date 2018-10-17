45587b35d9a68fe0d4781bb5a7939fd6a704efa6
Jeff Chasin <jeff@jeffchasin.com>
2018-10-17 08:16:48 -0700

    stub out form endpoint routes

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -8,6 +8,21 @@ const port = 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/f1', (req, res) => {
+  console.log('/f1 req: ', req);
+  res.send('/f1 received');
+});
+
+app.get('/f2', (req, res) => {
+  console.log('/f2 req: ', req);
+  res.send('/f2 received');
+});
+
+app.get('/f3', (req, res) => {
+  console.log('/f3 req: ', req);
+  res.send('/f3 received');
+});
+
 app.listen(port, () => {
   console.log('Express listening at: http://localhost:' + port);
 });
