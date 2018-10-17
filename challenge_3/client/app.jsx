5acf2e1f01127d6f1a182b2b8aecf81779f3455d
Jeff Chasin <jeff@jeffchasin.com>
2018-10-16 19:23:38 -0700

    add F1 and F2 fields to components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,15 +1,59 @@
 
 var Form = function (props) {
   return (
-    <button onClick={props.checkout}>
-      Checkout
+    <button
+      className='btn btn-primary btn-lg btn-block'
+      onClick={props.checkout}>
+      Checkout Now
     </button>
   );
 };
 
 var F1 = function (props) {
   return (
-    <div>This is F1</div>
+    <form>
+      <div className='mb-3'>
+        <label for='name'>Name</label>
+        <input type='text' className='form-control' id='name' placeholder='' value='' />
+      </div>
+      <div className='mb-3'>
+        <label for='email'>Email</label>
+        <input type='email' className='form-control' id='email' placeholder='you@example.com' value='' />
+      </div>
+      <div className='mb-3'>
+        <label for='password'>Password</label>
+        <input type='password' className='form-control' id='password' placeholder='' />
+      </div>
+    </form>
+  );
+};
+
+var F2 = function (props) {
+  return (
+    <form>
+      <div className='mb-3'>
+        <label for='address'>Address</label>
+        <input type='text' className='form-control' id='address' />
+      </div>
+      <div className='mb-3'>
+        <label for='address2'>Address 2</label>
+        <input type='text' className='form-control' id='address2' />
+      </div>
+      <div className='row'>
+        <div className='col-md-5 mb-3'>
+          <label for='city'>City</label>
+          <input type="text" className='form-control' id='city' />
+        </div>
+        <div className='col-md-4 mb-3'>
+          <label for='state'>State</label>
+          <input type="text" className='form-control' id='state' />
+        </div>
+        <div className='col-md-3 mb-3'>
+          <label for='zip'>Zip</label>
+          <input type='text' className='form-control' id='zip' placeholder='' />
+        </div>
+      </div>
+    </form>
   );
 };
 
@@ -39,6 +83,10 @@ class Checkout extends React.Component {
     } else if (this.state.isF1 === true) {
       return (
         <F1 />
+      );
+    } else if (this.state.isF2 === true) {
+      return (
+        <F2 />
       );
     }
   }
