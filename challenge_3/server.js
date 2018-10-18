c1d209f7c41f55d5c3a01b55f4280ff90e2e4f01
Jeff Chasin <jeff@jeffchasin.com>
2018-10-17 19:25:13 -0700

    test mySQL connection

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
 const app = express();
 const port = 3000;
 
 var formParser = bodyParser.urlencoded({ extended: true });
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  operatorsAliases: false
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('mySQL Connection established!');
+  })
+  .catch((err) => {
+    console.error('mySQL DB connection error:', err);
+  });
 
 app.use(express.static(__dirname + '/public'));
 
