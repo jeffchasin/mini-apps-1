a7d89043f289a6fa2f65133bd93540af495b32fa
Jeff Chasin <jeff@jeffchasin.com>
2018-10-23 18:52:54 -0700

    html form sending, server receiving

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,5 +3,11 @@ const app = express();
 const port = 3000;
 
 app.use('/', express.static('client'));
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', function (req, res) {
+  console.log(req.body.jsonInput);
+  res.send(req.body.jsonInput);
+});
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
